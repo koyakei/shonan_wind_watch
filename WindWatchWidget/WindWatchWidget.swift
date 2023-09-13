e582afa9bf10b7415b83d1b6e6b0b873787d4064
koyanagi <koyakeiaaaaa@gmail.com>
2023-09-13 15:32:15 +0900

    complecation 問い合わせ頻度高すぎ

--- WindWatchWidget/WindWatchWidget.swift
+++ WindWatchWidget/WindWatchWidget.swift
@@ -6,31 +6,71 @@
 //
 
 import WidgetKit
+import WatchKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
+    func getTimeline(in context: Context, completion: @escaping (Timeline<SimpleEntry>) -> Void) {
+        URLSession.shared.dataTask(with: URL(string: "https://mysite-906r.onrender.com/wind_speed_hayamas/1.json")!) {(data, response, error) in
+            var entries : [SimpleEntry] = []
+            if let _ = error {
+                print("error")
+              return
+            }
+            if let response = response as? HTTPURLResponse {
+                
+              if (200...399).contains(response.statusCode) {
+                  let wind = (try! JSONSerialization.jsonObject(with: data!) as! [String: Any])["wind_speed_meter_per_second"] as! Float
+                  
+                  let entry = SimpleEntry(date: Date.now, windSpeed:wind)
+                  entries.append(entry)
+                  let timeline = Timeline(entries: entries, policy:
+                        .after(Calendar.current.date(byAdding: .minute, value: 15, to: entry.date)!
+                              )
+                                                                          )
+                  completion(timeline)
+              } else {
+                  print(response.statusCode)
+              }
+            }
+        }.resume()
+    }
+    
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date.now, windSpeed: 11)
+//        semaphore = DispatchSemaphore(value: 0)
+        var request = URLRequest(url: URL(string: "https://mysite-906r.onrender.com/wind_speed_hayamas/1.json")!)
+                request.httpMethod = "GET"
+
+                                           // HTTPリクエスト実行
+                                           let res = URLSession.shared.dataTask(with: request).resume()
+                                           // requestCompleteHandler内でsemaphore.signal()が呼び出されるまで待機する
+        print(res)
+                                           
+        return SimpleEntry(date: Date.now, windSpeed:12)
     }
 
     func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date.now, windSpeed: 11)
         completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: Date.now, windSpeed: 11)
-            entries.append(entry)
+        
+        URLSession.shared.dataTask(with: URL(string: "https://mysite-906r.onrender.com/wind_speed_hayamas/1.json")!) {(data, response, error) in
+            if let _ = error {
+                print("error")
+                return
+            }
+            if let response = response as? HTTPURLResponse {
+                
+                if (200...399).contains(response.statusCode) {
+                    let wind = (try! JSONSerialization.jsonObject(with: data!) as! [String: Any])["wind_speed_meter_per_second"] as! Float
+                    
+                    let entry = SimpleEntry(date: Date.now, windSpeed:wind)
+                    completion(entry)
+                } else {
+                    print(response.statusCode)
+                }
+            }
+            //        }.resume()
         }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
     }
 }
 
@@ -44,10 +84,11 @@ struct WindWatchWidgetEntryView : View {
 
     var body: some View {
         VStack {
-            HStack {
-                Text(String(entry.windSpeed))
+            Text(String(entry.windSpeed)).task {
+                WidgetCenter.shared.reloadTimelines(ofKind: "WindWatchWidget")
             }
-        }}
+        }
+    }
 }
 
 @main
@@ -56,8 +97,8 @@ struct WindWatchWidget: Widget {
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-                WindWatchWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
+            WindWatchWidgetEntryView(entry: entry)
+                .containerBackground(.fill.tertiary, for: .widget)
             
         }
         .configurationDisplayName("My Widget")
@@ -68,6 +109,6 @@ struct WindWatchWidget: Widget {
 #Preview(as: .accessoryRectangular) {
     WindWatchWidget()
 } timeline: {
-    SimpleEntry(date: .now, windSpeed: 11)
-    SimpleEntry(date: .now, windSpeed: 11)
+    SimpleEntry(date: .now, windSpeed: 14)
+    SimpleEntry(date: .now, windSpeed: 15)
 }
